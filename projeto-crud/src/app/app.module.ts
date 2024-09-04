103aa92800a7c5b135c86bf80f42b4e30915973a
anapppp <anapaula.ksoares@gmail.com>
2024-09-04 20:44:05 -0300

    modulo aluno criado

--- projeto-crud/src/app/app.module.ts
+++ projeto-crud/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunoModule } from './aluno/aluno.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlunoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
