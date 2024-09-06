e4b5d17cfaf120e227cb117f0f45aa6e3def7cd4
anapppp <anapaula.ksoares@gmail.com>
2024-09-05 22:37:03 -0300

    feat: listar-aluno-component implementado

--- projeto-crud/src/app/shared/models/aluno.model.ts
+++ projeto-crud/src/app/shared/models/aluno.model.ts
@@ -1,8 +1,8 @@
 export class Aluno {
     // id – numérico• nome – string• cpf – string• e - mail – string• data de nascimento – string
     constructor(
-        public id: number,
-        public nome: string,
+        public id?: number,
+        public nome?: string,
         public cpf?: string,
         public email?: string,
         public dateNascimento?: string
