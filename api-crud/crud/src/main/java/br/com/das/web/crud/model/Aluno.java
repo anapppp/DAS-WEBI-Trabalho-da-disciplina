1bcfff979ec2f741077b64b7b2cbe0a8c14fdc5a
anapppp <anapaula.ksoares@gmail.com>
2024-10-05 16:21:23 -0300

    model Aluno.java feito

--- api-crud/crud/src/main/java/br/com/das/web/crud/model/Aluno.java
+++ api-crud/crud/src/main/java/br/com/das/web/crud/model/Aluno.java
@@ -1,7 +1,60 @@
 package br.com.das.web.crud.model;
 
 public class Aluno {
-    // private int id;
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String dataNascimento;
 
-    
+    public Aluno() {
+    };
+
+    public Aluno(int id, String nome, String cpf, String email, String dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
